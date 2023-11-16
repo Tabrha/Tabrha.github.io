606c5d96ad4c1b7dfe30445d9e87278935bc09a6
Tabrha <tesfayeabrha95@gmail.com>
2023-11-16 15:16:18 -0600

    function added

--- SD415/D33Closure/dist/src/closures.js
+++ SD415/D33Closure/dist/src/closures.js
@@ -1,21 +1,3 @@
-/**
- *
- * @param {Array} arr holding values
- * @returns {Function} returns a function that tests whether its argument is in the arr
- *
- */
-/**
- *
- * @param {number} low is bottom of range
- * @param {number}  high is top of range
- * @returns {Function} returns a function that tests whether its argument is inside the range
- *
- */
-/**
- * @returns {Function} closure that returns it's number
- */
-//army[0](); // the shooter number 0 shows 10
-//army[5](); // and number 5 also outputs 10...
 export { inArray, inBetween, makeArmy };
 function inArray(arr) {
     return function (input) {
@@ -38,3 +20,5 @@ function makeArmy() {
     }
     return army;
 }
+//army[0](); // the shooter number 0 shows 10
+//army[5](); // and number 5 also outputs 10...
--- SD415/D33Closure/dist/src/shoppingCart.js
+++ SD415/D33Closure/dist/src/shoppingCart.js
@@ -1,36 +1,18 @@
 //This closure exercise involves a common real-world scenario.  
-// This exercise uses closures to create a shopping cart. It encapsulates the cart array, allowing you to
-//  add items, remove items, and calculate the total cost. The items and their prices are private and can only be 
-//  accessed or modified through the provided methods, demonstrating the concept of closures in a different context.
-// Exercise: Closure and a Shopping Cart
-// Create a function that simulates a shopping cart. The cart should allow you to add items,
-// remove items, and calculate the total cost. Items and their prices should be private
-// and not directly accessible.
-// type Item = {
-//     item: string;
-//     price: number;
-// }
-// type Cart = {
-//     addItem: (item: string, price: number) => void;
-//     removeItem: (item: string) => void;
-//     getTotal: () => number;
-// }
 export function createShoppingCart() {
-    export function createShoppingCart() {
-        const cartItems = [];
-        return {
-            addItem: (item, price) => {
-                cartItems.push({ item, price });
-            },
-            removeItem: (item) => {
-                const index = cartItems.findIndex((cartItem) => cartItem.item === item);
-                if (index !== -1) {
-                    cartItems.splice(index, 1);
-                }
-            },
-            getTotal: () => {
-                return cartItems.reduce((total, currentItem) => total + currentItem.price, 0);
-            },
-        };
-    }
+    const cartItems = [];
+    return {
+        addItem: (item, price) => {
+            cartItems.push({ item, price });
+        },
+        removeItem: (item) => {
+            const index = cartItems.findIndex((cartItem) => cartItem.item === item);
+            if (index !== -1) {
+                cartItems.splice(index, 1);
+            }
+        },
+        getTotal: () => {
+            return cartItems.reduce((total, currentItem) => total + currentItem.price, 0);
+        },
+    };
 }
